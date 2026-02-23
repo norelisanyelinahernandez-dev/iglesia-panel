b8fac2403858d18717bdc527f24e23f2d893d3d1
norelisanyelinahernandez-dev <norelisanyelinahernandez@gmail.com>
2026-02-22 21:46:48 -0400

    agregar PWA manifest y service worker

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+﻿const CACHE = "iglesia-v1"
+const ARCHIVOS = [
+  "/",
+  "/index.html",
+  "/logo.jpg"
+]
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
+  )
+})
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+})
