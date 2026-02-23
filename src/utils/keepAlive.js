96f32611417200af1a17b446641931c6b8a6b7d9
norelisanyelinahernandez-dev <norelisanyelinahernandez@gmail.com>
2026-02-22 21:13:26 -0400

    agregar keepAlive para mantener backend despierto

---
+++ src/utils/keepAlive.js
@@ -0,0 +1,9 @@
+﻿const BACKEND_URL = 'https://iglesia-api-g1ux.onrender.com'
+
+export const iniciarKeepAlive = () => {
+  const ping = () => {
+    fetch(`${BACKEND_URL}/health`).catch(() => {})
+  }
+  ping()
+  setInterval(ping, 10 * 60 * 1000)
+}
